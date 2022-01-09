ab781e18858ef72806c0a0d8ff780157d63ef52e
naebakang <pjbtkk@gmail.com>
2022-01-09 23:21:11 +0900

    add, component

---
+++ app.py
@@ -0,0 +1,17 @@
+# File encoding: UTF- 8
+
+from flask import Flask
+
+from component.html_factory import Base
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def index():
+    ins_base = Base()
+    return ins_base.get_html()
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
---
+++ templates/index.py
@@ -0,0 +1,14 @@
+# File encoding: UTF-8
+def test():
+    return '''
+        <!doctype html>
+            <html>
+                <head>
+                    <title>Welcome</title>
+                    <meta charset="utf-8">
+                </head>
+                <body>
+                    hello!
+                </body>
+            </html>
+        '''
